688813f7e65c98353400c516f4b8c1aca0d2330a
karenlopez-13 <karenlopez91931@gmail.com>
2021-05-29 16:43:30 -0500

    Practica completa

--- AutentificacionFB/scripts/autorizacion.js
+++ AutentificacionFB/scripts/autorizacion.js
@@ -1,4 +1,18 @@
+auth.onAuthStateChanged( user => {
+    if(user){
+        db.collection('platillos').onSnapshot(snapshot => {
+            obtienePlatillos(snapshot.docs);
+        });
+        configurarMenu(user);
+    }
+    else{
+        obtienePlatillos([]);
+        configurarMenu();
+    }
+});
+
 const formaingresar = document.getElementById("formaingresar");
+
 formaingresar.addEventListener('submit', (e) =>{
     e.preventDefault();
     let correo = formaingresar['correo'].value;
@@ -36,7 +50,53 @@ function messageError(codigo){
 const salir = document.getElementById("salir");
 salir.addEventListener('click', (e) =>{
     e.preventDefault();
-    auth.signOut().then( ()=>{
+    auth.signOut().then(() => {
         alert('Se ha cerrado sesión');
     });
 });
+
+const formaregistrate = document.getElementById('formaregistrate');
+
+formaregistrate.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const correo = formaregistrate['rcorreo'].value;
+    const contrasena = formaregistrate['rcontrasena'].value;
+
+    auth.createUserWithEmailAndPassword(correo, contrasena).then(cred =>{
+        //mandar llamar la base de datos
+        return db.collection('usuarios').doc(cred.user.uid).set({
+            nombre: formaregistrate['rnombre'].value,
+            telefono: formaregistrate['rtelefono'].value,
+            direccion: formaregistrate['rdireccion'].value
+        });
+    }).then( ()=> {
+        $('#registatemodal').modal('hide');
+        formaregistrate.reset();
+        formaregistrate.querySelector('.error').innerHTML = '';
+    }).catch( err => {
+        formaregistrate.querySelector('.error').innerHTML = messageError(err.code);
+    });
+});
+
+entrarGoogle = () =>{
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+        var token = result.credential.accessToken;
+        console.log(token);
+
+        var user = result.user;
+        let html = `
+        <p> Nombre: ${user.displayName}</p>
+        <p> Correo: ${user.email}</p>
+        <img src="${user.photoURL}"></img>
+        `;
+
+        datosdelacuenta.innerHTML = html;
+        $('#ingresarmodal').modal('hide');
+
+        formaingresar.reset();
+        formaingresar.querySelector('.error').innerHTML = '';
+    }).catch(function(error){
+        console.log("Error " + error)
+    });
+}
